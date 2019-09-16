33e357193fb50f96144499ca59b011b867f8b44a
William Sedlacek <wsedlacekc@gmail.com>
2019-09-16 14:38:41 -0500

    Setup basic router

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './app.component';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
